6751a8e49e344bc1fc2852e196593fe99b64c8e7
KFEDGRANT <38848126+KFEDGRANT@users.noreply.github.com>
2019-05-13 13:08:31 -0400

    Update docx_to_txt.py

--- docx_to_txt.py
+++ docx_to_txt.py
@@ -1,11 +1,21 @@
 #https://automatetheboringstuff.com/chapter13/
 #write docx file to txt
+
+#pull text from .docx
+def getText(filename):
+    doc = Document(filename)
+    fullText = []
+    for para in doc.paragraphs:
+        fullText.append(para.text)
+    return '\n'.join(fullText)
+
+#write text to .txt
 directory = #filepath
 for filename in os.listdir(directory):
-    if filename.endswith(\".docx\"):
+    if filename.endswith(".docx"):
         fulltext = getText(filename)
         filename.rsplit('.')
-        f= open(filename+\".txt\",\"w+\")
+        f = open(filename+".txt","w+")
         f.write(fulltext)
     else:
         continue
